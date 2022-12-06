f89b2cda37c7437bb23bc91756ad8b77e45ea11c
Arul Frances <arulfrances@gmail.com>
2022-12-06 23:02:18 +0530

    Fixed Form - Controls

--- client/src/components/InputProduct.js
+++ client/src/components/InputProduct.js
@@ -12,8 +12,8 @@ const InputProducts = () => {
             <h1 className="text-center mt-5">Praise be to LORD</h1>
             <form className="d-flex mt-5">
                 <input type="text" className="form-control" />
-                <input type="text" className="form-control1" />
-                <input type="text" className="form-control2" />
+                <input type="text" className="form-control" />
+                <input type="text" className="form-control" />
                 <button className="btn btn-success">Add Product</button>
             </form>
         </Fragment>
