e875df4390ec1b97159f30d4040874aeebfb7e16
Arul Frances <arulfrances@gmail.com>
2022-12-06 22:58:37 +0530

    Updated Input Products with Input Fields

--- client/src/components/InputProduct.js
+++ client/src/components/InputProduct.js
@@ -9,7 +9,7 @@ const InputProduct = () => {
 const InputProducts = () => {
     return (
         <Fragment>
-            <h1 className="text-center mt-5">Praise be to LORD!</h1>
+            <h1 className="text-center mt-5">Praise be to LORD</h1>
             <form className="d-flex mt-5">
                 <input type="text" className="form-control" />
                 <input type="text" className="form-control1" />
@@ -20,4 +20,4 @@ const InputProducts = () => {
     )
 }
 
-export default InputProduct;
+export default InputProducts;
