8acdbb66ee06467cdf666e872b1d712b7cadaa7f
Arul Frances <arulfrances@gmail.com>
2022-12-06 21:58:39 +0530

    Added CRUD APIs in index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,89 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 //middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //req.body
 
 
+//routes
+
+//create a product
+app.post("/products", async (req, res) => {
+    try {
+        console.log(req.body);
+        const { productname, description, price } = req.body;
+
+        const newProduct = await pool.query("INSERT INTO producttable ( productname, description, price) VALUES ($1,$2,$3) RETURNING *",
+            [productname, description, price]);
+
+        res.json(newProduct.rows[0]);
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+
+//get all products
+
+app.get("/products", async (req, res) => {
+    try {
+        const allProducts = await pool.query("SELECT * from producttable");
+        res.json(allProducts.rows);
+    } catch (error) {
+        console.log(error.message);
+    }
+})
+
+//get a product
+
+app.get("/product/:id", async (req, res) => {
+    try {
+        //console.log(req.params);
+        const { id } = req.params;
+        const product = await pool.query("SELECT *  FROM producttable WHERE productid = $1", [id])
+
+        res.json(product.rows[0]);
+
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+//update a product
+
+app.put("/product/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { price } = req.body;
+        const productUpdate = await pool.query("UPDATE producttable SET price = $2 WHERE productid  = $1", [id, price])
+
+        res.json("Product Name updated")
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+//delete a product
+
+app.delete("/product/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const productDelete = await pool.query("DELETE from productTable WHERE productid = $1", [id]);
+
+        res.json("Product Deleted");
+
+        console.log("Product Deleted");
+
+    } catch (error) {
+
+    }
+})
 
 app.listen(5000, () => {
     console.log("Praise be to LORD!");
