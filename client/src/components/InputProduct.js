db4a2e3c59b565b6f61d938c22be4d18ec4b2f6c
Arul Frances <arulfrances@gmail.com>
2022-12-07 20:24:01 +0530

    Back to same screen

--- client/src/components/InputProduct.js
+++ client/src/components/InputProduct.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const InputProduct = () => {
     return (
@@ -7,17 +7,45 @@ const InputProduct = () => {
 }
 
 const InputProducts = () => {
+
+    const [productname, setProductName] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPriceValue] = useState("");
+
+    const onSubmitForm = async e => {
+        e.preventDefault();
+
+        try {
+
+            const body = { productname, description, price };
+            const response = await fetch("http://localhost:5000/products", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body)
+            });
+            window.location = "/";
+            console.log(response);
+
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
     return (
         <Fragment>
             <h1 className="text-center mt-5">Praise be to LORD</h1>
-            <form className="d-flex mt-5">
-                <input type="text" className="form-control" />
-                <input type="text" className="form-control" />
-                <input type="text" className="form-control" />
+            <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+                <input type="text" className="form-control" value={productname}
+                    onChange={e => setProductName(e.target.value)} />
+                <input type="text" className="form-control" value={description}
+                    onChange={e => setDescription(e.target.value)} />
+                <input type="text" className="form-control" value={price}
+                    onChange={e => setPriceValue(e.target.value)} />
                 <button className="btn btn-success">Add Product</button>
             </form>
         </Fragment>
     )
 }
+
 
 export default InputProducts;
